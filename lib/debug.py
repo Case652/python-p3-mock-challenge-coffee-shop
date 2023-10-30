b6aed854fab14e39ec6d45670dacac56521b7b48
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-10-30 13:59:25 -0500

    attempted and reviewed

--- lib/debug.py
+++ lib/debug.py
@@ -7,5 +7,12 @@ from classes.many_to_many import Coffee
 
 if __name__ == '__main__':
     print("HELLO! :) let's debug")
-
-    ipdb.set_trace()
+c1 = Coffee("Mocha")
+c2 = Coffee("Dry")
+CU1 = Customer("Bill")
+CU2 = Customer("Benny")
+O1 = Order(CU1,c1,1.99)
+O2 = Order(CU1,c2,2.99)
+O3 = Order(CU2,c1,3.99)
+O4 = Order(CU2,c1,4.99)
+ipdb.set_trace()
